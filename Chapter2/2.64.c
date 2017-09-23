8e855810263563431d3d4a9c8583fcdc2f3deedd
alongwy <15939423778@163.com>
2017-09-23 19:44:01 +0800

    感谢章鱼哥的提醒 :)

--- Chapter2/2.64.c
+++ Chapter2/2.64.c
@@ -10,28 +10,7 @@
  * @return
  */
 int any_odd_one(unsigned x) {
-
-    unsigned bit_1 = (x & 0x01);
-    unsigned bit_3 = ((x >> 2) & 0x01);
-    unsigned bit_5 = ((x >> 4) & 0x01);
-    unsigned bit_7 = ((x >> 6) & 0x01);
-    unsigned bit_9 = ((x >> 8) & 0x01);
-    unsigned bit_11 = ((x >> 10) & 0x01);
-    unsigned bit_13 = ((x >> 12) & 0x01);
-    unsigned bit_15 = ((x >> 14) & 0x01);
-    unsigned bit_17 = ((x >> 16) & 0x01);
-    unsigned bit_19 = ((x >> 18) & 0x01);
-    unsigned bit_21 = ((x >> 20) & 0x01);
-    unsigned bit_23 = ((x >> 22) & 0x01);
-    unsigned bit_25 = ((x >> 24) & 0x01);
-    unsigned bit_27 = ((x >> 26) & 0x01);
-    unsigned bit_29 = ((x >> 28) & 0x01);
-    unsigned bit_31 = ((x >> 30) & 0x01);
-
-    return bit_1 | bit_3 | bit_5 | bit_7 | bit_9
-           | bit_11 | bit_13 | bit_15 | bit_17
-           | bit_19 | bit_21 | bit_23 | bit_25
-           | bit_27 | bit_29 | bit_31;
+    return (int) ((x & 0x55555555) != 0);
 }
 
 int main() {
