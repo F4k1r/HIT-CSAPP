55bca9224077b4ada0de2b5b1153afeced7d6230
alongwy <15939423778@163.com>
2017-09-23 19:19:54 +0800

    add README.md update some homework

--- Chapter2/2.80.c
+++ Chapter2/2.80.c
@@ -10,13 +10,13 @@
  * @return （3/4）*x
  */
 int threefourths(int x) {
+
     int a = x >> 1;
     int b = x >> 2;
     int res = a + b;
 
     // 当x的低两位为0x03的时候,损失会很大所以+1补充
-    if ((x & 0x03) == 0x03)
-        ++res;
+    res += (int) ((x & 0x03) == 0x03);
 
     return res;
 }
