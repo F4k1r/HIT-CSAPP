8f3723c332287dfd4f207a0cceda10cabba9ed00
alongwy <15939423778@163.com>
2017-09-28 09:23:57 +0800

    update 2.96

--- Chapter2/2.96.c
+++ Chapter2/2.96.c
@@ -3,7 +3,6 @@
 //
 
 #include <stdio.h>
-
 typedef unsigned float_bits;
 
 /**
@@ -14,22 +13,27 @@ typedef unsigned float_bits;
  * @return
  */
 int float_f2i(float_bits f);
-
+float U2F_BIT(unsigned U) {
+    return *((float *) &U);
+}
 int main() {
-    for (float_bits i = 0; i != 0xFFFFFFF; ++i) {
-        printf("%d\n", float_f2i(i));
+    for (float_bits i = 0; i != 0xFF; ++i) {
+        printf("%d   |  %d\n", (int) U2F_BIT(i), float_f2i(i));
     }
 
     return 0;
 }
-
 int float_f2i(float_bits f) {
     unsigned sign = f >> 31;
     unsigned exp = f >> 23 & 0xFF;
     unsigned frac = f & 0x7FFFFF;
-
     if (exp == 0xFF && frac != 0)
         return 0x80000000;
-    else
-        return ((sign) << 31) | (((1 << 23) | frac) >> (32 - (exp - 126 - 23)));
+    else if (exp > 150) {
+        frac = ((1 << 23) | frac) << ((exp - 150));
+        return sign ? -frac : frac;
+    } else {
+        frac = ((1 << 23) | frac) >> ((150 - exp));
+        return sign ? -frac : frac;
+    }
 }
