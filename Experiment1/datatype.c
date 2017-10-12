3d72d1e2ee152359a442adda3d54f313546dd3f6
alongwy <15939423778@163.com>
2017-10-12 09:08:54 +0800

    update datatype.c

--- Experiment1/datatype.c
+++ Experiment1/datatype.c
@@ -1,5 +1,7 @@
 //
 // Created by along on 17-10-10.
+// 包含C各种数据类型，包括指针、数组、struct、union、enum、函数main的地址、printf的地址等。
+// 打印每个变量名、内容、地址、对应16进制的内存各字节
 //
 
 #include <stdio.h>
@@ -14,27 +16,48 @@ void show_bytes(byte_pointer start, size_t len) {
     printf("\n");
 }
 
-const char *mstring = "冯云龙";
-const int intenger = 1160300202;
-const long lintenger = 1160300202;
-const long long llintenger = 1160300202;
-const float floatnum = 411627199805247416.f;
-const double doublenum = 411627199805247416;
+char *mstring = "冯云龙";
+int intenger = 1160300202;
+long lintenger = 1160300202;
+long long llintenger = 1160300202;
+float floatnum = 411627199805247416.f;
+double doublenum = 411627199805247416;
+int *pInt = &intenger;
+int intArray[10];
+struct StructData {
+    int aInt;
+    char aChar;
+} structData;
+union UnionData {
+    unsigned uData;
+    float floatData;
+} unionData;
+enum {
+    Red, Yellow, Blue
+} color = Blue;
 
 //变量名、变量值、变量地址、变量对应16进制的内存各字节。
 int main() {
-    printf("变量名:mstring     变量值:%s   变量地址:%p  内存:", mstring, &mstring);
-    show_bytes((byte_pointer) mstring, strlen(mstring));
-    printf("变量名:intenger    变量值:%d   变量地址:%p  内存:", intenger, &intenger);
+    printf("变量名:mstring     变量值:%s   变量地址:%p  内存:",
+           mstring, &mstring);
+    show_bytes((byte_pointer) &mstring, strlen(mstring));
+    printf("变量名:intenger    变量值:%d   变量地址:%p  内存:",
+           intenger, &intenger);
     show_bytes((byte_pointer) &intenger, sizeof(intenger));
-    printf("变量名:lintenger   变量值:%ld  变量地址:%p  内存:", lintenger, &lintenger);
+    printf("变量名:lintenger   变量值:%ld  变量地址:%p  内存:",
+           lintenger, &lintenger);
     show_bytes((byte_pointer) &lintenger, sizeof(lintenger));
-    printf("变量名:llintenger  变量值:%lld 变量地址:%p  内存:", llintenger, &llintenger);
+    printf("变量名:llintenger  变量值:%lld 变量地址:%p  内存:",
+           llintenger, &llintenger);
     show_bytes((byte_pointer) &llintenger, sizeof(llintenger));
-    printf("变量名:floatnum    变量值:%f   变量地址:%p  内存:", floatnum, &floatnum);
+    printf("变量名:floatnum    变量值:%f   变量地址:%p  内存:",
+           floatnum, &floatnum);
     show_bytes((byte_pointer) &floatnum, sizeof(floatnum));
-    printf("变量名:doublenum   变量值:%lf  变量地址:%p  内存:", doublenum, &doublenum);
+    printf("变量名:doublenum   变量值:%lf  变量地址:%p  内存:",
+           doublenum, &doublenum);
     show_bytes((byte_pointer) &doublenum, sizeof(doublenum));
-
+    printf("变量名:pInt        变量值:%p  变量地址:%p  内存:",
+           pInt, &pInt);
+    show_bytes((byte_pointer) &pInt, sizeof(pInt));
     return 0;
 }
